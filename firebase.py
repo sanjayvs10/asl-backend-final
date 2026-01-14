0af77be6b20a23c36316f5ce251ca1eacf2cc52a
sanjayvs10 <sanjayvs172@gmail.com>
2026-01-14 19:30:26 +0530

    Fix firebase env usage

--- firebase.py
+++ firebase.py
@@ -1,17 +1,16 @@
-import firebase_admin
-import json
 import os
-from firebase_admin import credentials, db, firestore
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
 
+# Load Firebase key from ENV variable
 firebase_key = json.loads(os.environ.get("FIREBASE_KEY"))
 
-cred = credentials.Certificate("firebase_key")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://asl-auction-management-system-default-rtdb.firebaseio.com/'
-})
+cred = credentials.Certificate(firebase_key)
+firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-players_ref = db.reference("players")
-teams_ref = db.reference("teams")
-users_ref = db.reference("users")
+players_ref = db.collection("players")
+teams_ref = db.collection("teams")
+users_ref = db.collection("users")
